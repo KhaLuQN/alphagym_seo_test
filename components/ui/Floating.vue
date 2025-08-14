54054c5dc63cb38f832997ab86e3805a78289706
KhaLuQN <khappp4@gmail.com>
2025-08-14 07:42:20 +0700

    debug

--- components/ui/Floating.vue
+++ components/ui/Floating.vue
@@ -9,7 +9,7 @@
         class="dropdown-content z-[1] menu p-2 shadow bg-black/95 rounded-box w-52 border border-red-600/30"
       >
         <li>
-          <a href="tel:0123456789" class="text-white hover:text-red-400">
+          <a href="tel:0386550651" class="text-white hover:text-red-400">
             <i class="fas fa-phone"></i> Gọi ngay
           </a>
         </li>
@@ -24,9 +24,9 @@
           </a>
         </li>
         <li>
-          <a href="#" class="text-white hover:text-red-400">
+          <NuxtLink to="/lien-he#ban-do" class="text-white hover:text-red-400">
             <i class="fas fa-map-marker-alt"></i> Chỉ đường
-          </a>
+          </NuxtLink>
         </li>
       </div>
     </div>
--- pages/lien-he/index.vue
+++ pages/lien-he/index.vue
@@ -1,8 +1,33 @@
 <template>
   <div>
     <ContactFrom />
+    <div class="py-4">
+      <div class="text-center bg-gradient-to-br from-gray-950 to-red-950">
+        <h2
+          class="text-5xl lg:text-6xl font-extrabold mb-4 text-red-400 custom-text-shadow"
+        >
+          Địa chỉ liên hệ
+        </h2>
+        <p class="text-xl text-gray-300 max-w-2xl mx-auto mb-8">
+          Sẵn sàng bắt đầu hành trình của bạn?
+        </p>
+
+        <!-- Bọc iframe trong container responsive -->
+        <div id="ban-do" class="w-full max-w-4xl mx-auto aspect-video pb-8">
+          <iframe
+            class="w-full h-full rounded-lg shadow-lg"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1442.3313681471902!2d108.16774589360053!3d16.052154954374437!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31421915a6c92fab%3A0xc894d3afcc1567a8!2zTmd1eeG7hW4gSHV5IFTGsOG7n25nLCBIw7JhIE1pbmgsIExpw6puIENoaeG7g3UsIMSQw6AgTuG6tW5nIDU1MDAwMCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1755130927694!5m2!1svi!2s"
+            style="border: 0"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script setup>
 useCustomSeoMeta({
   title: "Liên Hệ GymTech",
